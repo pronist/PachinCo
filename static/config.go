ebc978141203ec5a795150859b5f40f932388a96
pronist <pronist@naver.com>
2021-05-19 22:01:27 +0900

    Update Config default value

--- static/config.go
+++ static/config.go
@@ -15,10 +15,10 @@ var Config = struct {
 		AccessKey string `required:"true"` // 엑세스 키
 		SecretKey string `required:"true"` // 비밀 키
 	}
-	TradableBalanceRatio float64 `required:"true"` // 비중
-	OrderRatio           float64 `required:"true"` // 주문 가격 비중
+	TradableBalanceRatio float64 `default:"0.1"` // 비중
+	OrderRatio           float64 `default:"0.5"` // 주문 가격 비중
 	// 선택적으로 사용할 수 있는 옵션들
-	MaxTrackedMarket int      // 추적할 코인의 갯수 (분산투자 비율하고도 관련있음)
+	MaxTrackedMarket int      `default:"10"` // 추적할 코인의 갯수 (분산투자 비율하고도 관련있음)
 	Whitelist        []string // 마켓을 전략을 실행할 마켓을 수동으로 설정한다.
 	Blacklist        []string // 해당 마켓은 제외한다.
 }{}
